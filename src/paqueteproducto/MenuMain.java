240a1ad61a8647110b1dbfa008ff145230e322d1
JoelLeonUNS <202014033@uns.edu.pe>
2022-07-20 18:22:32 -0500

    Mensaje de prueba2

--- src/paqueteproducto/MenuMain.java
+++ src/paqueteproducto/MenuMain.java
@@ -3,7 +3,10 @@ package paqueteproducto;
 public class MenuMain {
 
     public static void main(String[] args) {
-        System.out.println("Prueba de GitHub");
+        System.out.println("Prueba de GitHub 2");
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
     }
     
     
