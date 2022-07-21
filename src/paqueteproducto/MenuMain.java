5d376366f7f276bc1396a2c85ea5ebc0b3b21fd1
AlexBD27 <202014034@uns.edu.pe>
2022-07-21 15:29:13 -0500

    borre todo lo que hicieron

--- src/paqueteproducto/MenuMain.java
+++ src/paqueteproducto/MenuMain.java
@@ -3,15 +3,7 @@ package paqueteproducto;
 public class MenuMain {
 
     public static void main(String[] args) {
-        System.out.println("Prueba de GitHub 2");
-        for (int i = 0; i < 10; i++) {
-            System.out.println(i);
-        }
-        for (int i = 10; i >0; i--) {
-            System.out.println(i);
-        }
-        System.out.println("Algo");
-        
+        System.out.println("BORRÉ TODO");
         
     }
     
