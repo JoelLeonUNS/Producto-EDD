03fb103157457438d7cdc09845534ae514489e32
JoelLeonUNS <202014033@uns.edu.pe>
2022-08-01 18:41:22 -0500

    Añadí un contador para los operadores

--- src/paqueteproducto/MenuMain.java
+++ src/paqueteproducto/MenuMain.java
@@ -43,8 +43,7 @@ public class MenuMain {
         System.out.println("\n\t-.-.-. INDICACIONES .-.-.-");
         System.out.println("- El tamaño de la pila y cola es la misma.");
         System.out.println("- Se debe completar la pila y cola.");
-        
-        
+
         System.out.print("\nDefina el tamaño de la pila y cola: ");
         longitud = input.nextInt();
 
@@ -60,28 +59,27 @@ public class MenuMain {
         llenarColaPila(c1, p1, longitud);
 
         // trasnformación
-        
         do {
             System.out.println("\nMENÚ: OPERADORES");
             System.out.println("1. Operadores con patrón: + - + - + ...");
             System.out.println("2. Operadores aleatorios.");
             System.out.println("3. Introducir por teclado los operadores.");
             System.out.print("Opción: ");
-            
+
             tipOp = input.nextInt();
             System.out.println((tipOp < 3 || tipOp > 1) ? "" : "\nError, opción no encontrada.\n");
         } while (tipOp > 3 || tipOp < 1);
-        
+
         do {
             System.out.println("\nMENÚ: EXPONENTE");
             System.out.println("1. Exponente descendente con excepción al último.");
             System.out.println("2. Introducir por teclado los exponentes.");
             System.out.print("Opción: ");
-            
+
             tipExp = input.nextInt();
             System.out.println((tipExp < 2 || tipExp > 1) ? "" : "\nError, opción no encontrada.\n");
         } while (tipExp > 2 || tipExp < 1);
-        
+
         System.out.println(transformacion(c1, p1, longitud, tipOp, tipExp));
     }
 
@@ -118,39 +116,39 @@ public class MenuMain {
         String notInf = "";
         String x, exp; // para almacenar el valor que retorna la cola al hacer la supresión
 
-            switch (tipExp) {
-                case 1 -> {
-                    for (int i = 0; i < longitud; i++) {
-                        x = c.suprimir();
-                        notInf += (x.equals("1") ? "" : x);
-                        notInf += (x.equals("1") ? "" : "*");
-                        notInf += p.pop(); // se hace pop y se disminuye en 1 el tope
-                        notInf += (p.getTope() == -1 || p.getTope() == 0 ? "" : "^");
-                        notInf += (p.getTope() == -1 || p.getTope() == 0 ? "" : p.getTope() + 1); // por eso se suma 1 al tope
-                        notInf += (i == longitud - 1 ? "" : operador(tipOp));
-                    }
-                }
-                case 2 -> {
-                    for (int i = 0; i < longitud; i++) {
-                        x = c.suprimir();
-                        notInf += (x.equals("1") ? "" : x);
-                        notInf += (x.equals("1") ? "" : "*");
-                        notInf += p.pop();
-                        System.out.print("Ingrese el exponente n°" + (1+i) + ": ");
-                        exp = input.next();
-                        notInf += (exp.equals("1") ? "" : "^");
-                        notInf += (exp.equals("1") ? "" : exp);
-                        notInf += (i == longitud - 1 ? "" : operador(tipOp));
-                    }
-                }
-                case 3 -> {
-
-                }
-                default -> {
-                    System.out.println("\nError, opción no encontrada.\n");
+        switch (tipExp) {
+            case 1 -> {
+                for (int i = 0; i < longitud; i++) {
+                    x = c.suprimir();
+                    notInf += (x.equals("1") ? "" : x);
+                    notInf += (x.equals("1") ? "" : "*");
+                    notInf += p.pop(); // se hace pop y se disminuye en 1 el tope
+                    notInf += (p.getTope() == -1 || p.getTope() == 0 ? "" : "^");
+                    notInf += (p.getTope() == -1 || p.getTope() == 0 ? "" : p.getTope() + 1); // por eso se suma 1 al tope
+                    notInf += (i == longitud - 1 ? "" : operador(tipOp));
                 }
             }
-            
+            case 2 -> {
+                for (int i = 0; i < longitud; i++) {
+                    x = c.suprimir();
+                    notInf += (x.equals("1") ? "" : x);
+                    notInf += (x.equals("1") ? "" : "*");
+                    notInf += p.pop();
+                    System.out.print("Ingrese el exponente n°" + (1 + i) + ": ");
+                    exp = input.next();
+                    notInf += (exp.equals("1") ? "" : "^");
+                    notInf += (exp.equals("1") ? "" : exp);
+                    notInf += (i == longitud - 1 ? "" : operador(tipOp));
+                }
+            }
+            case 3 -> {
+
+            }
+            default -> {
+                System.out.println("\nError, opción no encontrada.\n");
+            }
+        }
+
         return "\n" + notInf + "\n";
     }
 
@@ -166,8 +164,9 @@ public class MenuMain {
                 operador = (Math.round((Math.random() + 1)) == 1) ? "-" : "+";
             }
             case 3 -> {
-                System.out.print("Indroduzca el operador: ");
+                System.out.print("Indroduzca el operador n°" + (1 + itr) + ": ");
                 operador = String.valueOf(input.next().charAt(0));
+                itr++; // sirve para contar los operadores también.
             }
         }
         return operador;
